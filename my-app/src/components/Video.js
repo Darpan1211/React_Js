112614b497ceeedfd35b2feea4d8dabfee2fdfcd
Darpan1211 <darpan.innovate@gmail.com>
2023-06-29 16:39:40 +0530

    jsx style use in components

--- my-app/src/components/Video.js
+++ my-app/src/components/Video.js
@@ -1,7 +1,15 @@
+import './Video.css';
+
+let Topic = "React Js";
+let bgcolor = "bg";
 
 function Video(){
-    return <div>Video</div>
-}
+    return (
+    <>
+    <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+    <div className={bgcolor} style={{backgroundColor:'red'}}>{Topic} Tutorial- Components</div>
+  </>
+)}
 
 function Thumb(){
     return <div>Thumb</div>
