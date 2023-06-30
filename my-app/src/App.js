398c0f21c8605ddd447f45057e9797cd2c37da9d
Darpan1211 <darpan.innovate@gmail.com>
2023-06-30 16:31:59 +0530

    props create

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import {Video,Thumb} from './components/Video';
 function app(){
   return (
-    <div>
-  <div>Hello Dost</div>
-  <Video></Video>
-  <Thumb></Thumb>
+  <div>
+    <div>Hello Dost</div>
+    <Video title="node js tutorials"></Video>
+    <Video title="react ja tutorial"></Video>
+    <Thumb></Thumb>
   </div>
   )
 }
