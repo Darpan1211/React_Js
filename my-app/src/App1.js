c5118a81a47cc13815af08451e8aab20001e2781
Darpan1211 <darpan.innovate@gmail.com>
2023-07-01 13:42:47 +0530

    Create a Resume

---
+++ my-app/src/App1.js
@@ -0,0 +1,21 @@
+import Video from './components/Video';
+import './App.css'
+function app(){
+  let obj = {
+    title: 'Dark',
+    views: '15k',
+    time: '1 year ago',
+    channel: 'The Compass',
+    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
+  }
+  return (
+  <div className="app">
+    <div>Videos</div>
+    <Video title="Dark" views= "15k" time="1 year ago" channel="The Compass" url="https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg"></Video>
+    <Video bgColor="green" title="Wood" views= "10k"  time="1 month ago" channel="The Compass" url="https://marketplace.canva.com/EAFH3gODxw4/1/0/1131w/canva-black-%26-white-modern-mystery-forest-movie-poster-rLty9dwhGG4.jpg"></Video>
+    
+  </div>
+  )
+}
+
+export default app;
