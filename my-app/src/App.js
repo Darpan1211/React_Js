3ca56589b061f3adef80e6dfcdcacdb51ea55eee
Darpan1211 <darpan.innovate@gmail.com>
2023-07-03 16:17:38 +0530

    refactoring data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,8 @@
 import Video from './components/Video';
-import './App.css'
+import './App.css';
+import video from './data/data';
 function app(){
-  let video = [{
-    id : 1,
-    title: 'REact Js',
-    views: '15k',
-    time: '1 year ago',
-    channel: 'The Compass',
-    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
-    verified: true
-  },
-  {
-    id : 2,
-    title: 'Angular Js',
-    views: '15k',
-    time: '2 year ago',
-    channel: 'The Compass',
-    url: 'https://picsum.photos/id/1/200/300',
-    verified: false
-  },
-  {
-    id : 3,
-    title: 'Vue Js',
-    views: '15k',
-    time: '1.5 year ago',
-    channel: 'The Compass',
-    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
-    verified: true
-  },
-  {
-    id : 4,
-    title: 'Full Stack',
-    views: '15k',
-    time: '6 months ago',
-    channel: 'The Compass',
-    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
-    verified: false
-  }]
+  
   return (
   <div className="app">
     <div>Videos</div>
---
+++ my-app/src/data/data.js
@@ -0,0 +1,38 @@
+
+const video = [{
+    id : 1,
+    title: 'REact Js',
+    views: '15k',
+    time: '1 year ago',
+    channel: 'The Compass',
+    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
+    verified: true
+  },
+  {
+    id : 2,
+    title: 'Angular Js',
+    views: '15k',
+    time: '2 year ago',
+    channel: 'The Compass',
+    url: 'https://picsum.photos/id/1/200/300',
+    verified: false
+  },
+  {
+    id : 3,
+    title: 'Vue Js',
+    views: '15k',
+    time: '1.5 year ago',
+    channel: 'The Compass',
+    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
+    verified: true
+  },
+  {
+    id : 4,
+    title: 'Full Stack',
+    views: '15k',
+    time: '6 months ago',
+    channel: 'The Compass',
+    url: 'https://marketplace.canva.com/EAFVCFkAg3w/1/0/1131w/canva-red-and-black-horror-movie-poster-AOBSIAmLWOs.jpg',
+    verified: false
+  }]
+  export default video;
