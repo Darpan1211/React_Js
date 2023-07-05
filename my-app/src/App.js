101736754c366a09220e6fd7209a73e72cb455be
Darpan1211 <darpan.innovate@gmail.com>
2023-07-05 16:47:00 +0530

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,17 @@
-import Resume from './resume/Resume';
+import Video from './components/Video';
+import './App.css';
+import video from './data/data';
 function app(){
-  let obj = {
-    name: 'The Compass',
-    title:'Enroll as a student, Exit As a Professional',
-    url: 'compass.png',
-    phone:'9099994612',
-    email:'info@thecompass.co.in',
-    linkedin: 'https://www.linkedin.com/in/darpan-patel-india',
-    skill:['Full Stack Development','Data Science','UI/UX','Python'],
-    education:['Not Required'],
-    bio:'The Compass aims to create skilled professionals to work on futuristic projects in well-established national and international level companies.The students who pass out of The Compass, are assured with great placements. we have a clientele that understands the need for trained professionals. The Training will be on current & future market trends.'
-
-  }
+  
   return (
-  <Resume {...obj} ></Resume>
+  <div className="app">
+    <div>Videos</div>
+    {
+      video.map(video=><Video bgColor="green" key={video.id} title={video.title} views= {video.views}  time={video.time} channel={video.channel} url={video.url} verified={video.verified} id={video.id}></Video>
+      )
+    }
+    
+  </div>
   )
 }
 
