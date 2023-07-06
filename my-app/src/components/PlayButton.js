2522d8258770081325d6d93bb8ffbdd8baea8dfc
Darpan1211 <darpan.innovate@gmail.com>
2023-07-06 15:34:42 +0530

    auto button create

--- my-app/src/components/PlayButton.js
+++ my-app/src/components/PlayButton.js
@@ -1,13 +1,15 @@
-import './PlayButton.css';
-export default function PlayButton({children,message,onPlay,onPause}){
-    let playing = false;
-    function handleClick(){
-      if (playing) onPause();
-      else onPlay();
+import "./PlayButton.css";
+export default function PlayButton({ children, message, onPlay, onPause }) {
+  let playing = false;
+  function handleClick() {
+    if (playing) onPause();
+    else onPlay();
 
-      playing = !playing;
-    }
-    return(
-        <button onClick={handleClick}>{children} : {playing?'>':'||'}</button>
-    )
+    playing = !playing;
+  }
+  return (
+    <button onClick={handleClick}>
+      {children} : {playing ? ">" : "||"}
+    </button>
+  );
 }
